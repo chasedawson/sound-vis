805aa00f4529a54b66e9ed12e1202c68b393c4dd
Chase Dawson <s.c.dawson54@gmail.com>
2019-08-23 15:20:29 -0400

    cleaned up code

--- core.py
+++ core.py
@@ -2,38 +2,24 @@
 import numpy as np
 import math
 
-def reflectionOf(v1,v2):
+
+def reflection_of(v1, v2):
     part1 = np.dot(v1, v2)
-
     part2 = 2*part1
-
     part3 = part2*v2
-
-    reflectedVector = v1-part3
-
-    norm = reflectedVector/(np.linalg.norm(reflectedVector))
-
+    reflected_vector = v1-part3
+    norm = reflected_vector/(np.linalg.norm(reflected_vector))
     return norm
 
 
-
-for t in range (-10,10,1):
-
-    t/=10
-
-    f_x = ((9/8)**2*((9/8)*math.cos((1/4)+t)-(1/2)*math.sin((1/4)+math.pi/2+t)+math.cos((1/4)+t)))
-
-    f_y = ((9/8)**2*(-(9/8)*math.sin((1/4)+t)-(1/2)*math.sin((1/4)+math.pi/2+t)+math.cos((1/4)+t)))
-
-    f_z = -1
-
-
-    n = np.array([f_x,f_y,f_z])
-
-    v = np.array([0,3,-2])
-
-    vPrime = reflectionOf(v,n)
-
-    print()
-    print(vPrime)
-    print(np.linalg.norm(vPrime))
+def simulate_speaker():
+    for t in range(-10, 10, 1):
+        t /= 10
+        f_x = ((9/8)**2*((9/8)*math.cos((1/4)+t)-(1/2)*math.sin((1/4)+math.pi/2+t)+math.cos((1/4)+t)))
+        f_y = ((9/8)**2*(-(9/8)*math.sin((1/4)+t)-(1/2)*math.sin((1/4)+math.pi/2+t)+math.cos((1/4)+t)))
+        f_z = -1
+        n = np.array([f_x, f_y, f_z])
+        v = np.array([0, 3, -2])
+        v_prime = reflection_of(v, n)
+        print(v_prime)
+        print(np.linalg.norm(v_prime))
