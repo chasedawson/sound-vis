ea40f3408895575886ade3b5e03881ec45a6aaca
Chase Dawson <s.c.dawson54@gmail.com>
2019-08-23 15:11:16 -0400

    reflect function

--- core.py
+++ core.py
@@ -1,1 +1,9 @@
 # core file -- this is the file that will be run
+import helpers as hp
+import numpy as np
+
+v = np.array([1, 2, -1])
+n = np.array([0, -1, 1])
+
+v_ref = hp.reflect(v, n)
+print(v_ref)
